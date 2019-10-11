c0918bba51ebf94b2745eb26e639ecd3378a429d
Bmazon <1032297749@qq.com>
2019-10-11 15:29:04 +0800

    Update create-user.component.js

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Badge } from 'reactstrap';
-import { Spinner } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -45,13 +45,16 @@ export default class CreateUser extends Component {
       status: 'new'
     };
     
-    axios.post('http://localhost:4000/ideas/create', userObject).
+    if(userObject.ideaName && userObject.description && userObject.proposer && userObject.department)
+    {
+      axios.post('http://localhost:4000/ideas/create', userObject).
       then((res) => {
         console.log(res.data);
       }).catch((error) => {
         console.log(error);
       });
-    
+    }
+
     this.setState(
       { 
         name: '', 
@@ -68,24 +71,25 @@ export default class CreateUser extends Component {
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <Badge color="success">Idea Name</Badge >
-              <input type="text" value={this.state.name} onChange={this.onChangeUserName} className="form-control" />
+              <input type="text" value={this.state.name} onChange={this.onChangeUserName} className="form-control" placeholder="Give your idea a catching name"/>
           </div>
           <div className="form-group">
             <Badge color="success">Description</Badge>
-              <input type="text" value={this.state.description} onChange={this.onChangeDescription} className="form-control" />
+              <input type="text" value={this.state.description} onChange={this.onChangeDescription} className="form-control" placeholder="Summarize your idea in one sentence"/>
           </div>
           <div className="form-group">
             <Badge color="success">Proposer</Badge>
-              <input type="text" value={this.state.proposer} onChange={this.onChangeProposer} className="form-control" />
+              <input type="text" value={this.state.proposer} onChange={this.onChangeProposer} className="form-control" placeholder="E.g. San Zhang"/>
           </div>
           <div className="form-group">
             <Badge color="success">Department</Badge>
-              <input type="text" value={this.state.department} onChange={this.onChangeDepartment} className="form-control" />
+              <input type="text" value={this.state.department} onChange={this.onChangeDepartment} className="form-control" placeholder="E.g. MI Core Driver"/>
           </div>
-          <div className="form-group">
-            <input type="submit" value="Create idea" className="btn btn-success btn-block" />
-          </div>
-          <Spinner type="grow" color="success" />
+          <br>
+          </br>
+          <Button color="success" input type="submit">
+              Submit
+          </Button>
         </form>
       </div>
     )
